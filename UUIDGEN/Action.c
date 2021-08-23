0d07af0827a3053a3d484b79a5e5598ff0758e4f
VinitTeje <vinitteje@gmail.com>
2021-08-23 17:02:52 -0400

    Update Action.c

--- UUIDGEN/Action.c
+++ UUIDGEN/Action.c
@@ -4,7 +4,8 @@ Action()
 
 	// UUID example 9d77d210-0452-11ec-9a03-0242ac130003
 	
-	// Create UUID4 (Length of 4, values between 0000-ffff) and UUID8 (length of 8, values between 00000000-ffffffff ) randmom number parameters
+	// Create UUID4 (Length of 4, values between 0000-ffff) and UUID8 (length of 8, values between 00000000-ffffffff ) 
+	// randmom number parameters. %04lx, and %08lx
 	// Update value on each occurence
 	
 	lr_save_string(lr_eval_string("{UUID8}-{UUID4}-{UUID4}-{UUID4}-{UUID4}{UUID8}"), "UUID_P");
